052502623137c09aef01b6c478052cd0bf2c1717
Dudu <dudabyte@gmail.com>
2022-06-28 16:56:43 -0300

    Project Created

---
+++ UdemyMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UdemyMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
