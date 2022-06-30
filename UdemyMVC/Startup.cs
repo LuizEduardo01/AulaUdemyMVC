43c9c75177b5be62d846e7a40d5796b749680eb6
Dudu <dudabyte@gmail.com>
2022-06-30 16:40:51 -0300

    Changing theme

--- UdemyMVC/Startup.cs
+++ UdemyMVC/Startup.cs
@@ -37,9 +37,8 @@ namespace UdemyMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<UdemyMVCContext>(options =>
-                    options.UseMySql(Configuration.GetConnectionString("UdemyMVCContext"), builder =>
-                        builder.MigrationsAssembly("UdemyMVC")));
-            
+                   options.UseMySql(Configuration.GetConnectionString("UdemyMVCContext"), builder =>
+                     builder.MigrationsAssembly("UdemyMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
