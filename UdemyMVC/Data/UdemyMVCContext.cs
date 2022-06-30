13c0a70b31245ee76d666ceeb8cd6415c403354b
Dudu <dudabyte@gmail.com>
2022-06-30 08:15:42 -0300

    CRUD Scaffolding

---
+++ UdemyMVC/Data/UdemyMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UdemyMVC.Models;
+
+namespace UdemyMVC.Data
+{
+    public class UdemyMVCContext : DbContext
+    {
+        public UdemyMVCContext (DbContextOptions<UdemyMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UdemyMVC.Models.Department> Department { get; set; }
+    }
+}
