3dda2f2fd4bce9f1af39dc0293715e4903de6f18
Dudu <dudabyte@gmail.com>
2022-07-08 07:32:55 -0300

    Asynchronous operations using Tasks (async, await)

---
+++ UdemyMVC/Migrations/20220702225920_UpDate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UdemyMVC.Migrations
+{
+    public partial class UpDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ UdemyMVC/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyMVC.Models;
+
+namespace UdemyMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly UdemyMVCContext _context;
+
+        public DepartmentService(UdemyMVCContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Department>> FindAllAsync()
+        {
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
+        }
+    }
+}
